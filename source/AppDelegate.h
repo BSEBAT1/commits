332ba90b058c3f8d1c142ad42461cc5fb035183e
Berkay <Berkay_Sebat@yahoo.com>
2016-10-27 09:50:02 -0400

    Initial Commit

---
+++ source/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  source
+//
+//  Created by macbook pro on 10/27/16.
+//  Copyright © 2016 berkaysebat.com.sourcecontrol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ source/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  source
+//
+//  Created by macbook pro on 10/27/16.
+//  Copyright © 2016 berkaysebat.com.sourcecontrol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
